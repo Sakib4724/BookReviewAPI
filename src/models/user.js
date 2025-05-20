a28671fae71455f029c73dc03a4e35588cf507f0
Sakib <sakibshaikh2425@gmail.com>
2025-05-20 23:16:54 +0530

    User Model and Schema

---
+++ src/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 15
+    },
+    lastName: {
+        type: String
+    },
+    emailId: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+const User = mongoose.Model("User", userSchema);
+
+module.exports = User;
